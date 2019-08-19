d42aab61ecb4c38284a48c01a023e51eafc7a8a1
acukurs77 <ariana.cukurs1@gmail.com>
2019-08-19 22:13:30 +0000

    Lesson 24 Video Uploading Complete

--- app/controllers/instructor/lessons_controller.rb
+++ app/controllers/instructor/lessons_controller.rb
@@ -25,7 +25,7 @@ class Instructor::LessonsController < ApplicationController
 	end
 
 	def lessons_params
-		params.require(:lesson).permit(:title, :subtitle)
+		params.require(:lesson).permit(:title, :subtitle, :video)
 	end
 
 end
